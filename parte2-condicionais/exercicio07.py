47ad52f9560a960c36d687089f1a7d4148caf033
rafaelawitzke <rafaela_witzke@estudante.sesisenai.org.br>
2026-09-23 07:42:56 -0300

    Parte 2 exercicioc

---
+++ parte2-condicionais/exercicio07.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+    print("O maior número é:", numero1)
+elif numero2 > numero1:
+    print("O maior número é:", numero2)
+else:
+    print("Os números são iguais.")
---
+++ parte2-condicionais/exercicio10.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite a idade: "))
+
+if idade >= 16:
+    print("Já pode votar.")
+else:
+    print("Ainda não pode votar.")
